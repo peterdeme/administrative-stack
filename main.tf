550cc914687d79ed7cd3f57b515a4d63a2c559b2
Peter Deme <snypox@gmail.com>
2025-10-20 12:46:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,9 @@ resource "spacelift_context" "ctx1" {
   name     = "Context ${random_pet.pet1.id}"
   space_id = "sibling2-01K80HYFVQE1KX57Z628KDE9XJ"
 }
+
+resource "spacelift_policy" "no-weekend-deploys" {
+  name = "Policy ${random_pet.pet1.id}"
+  body = "package spacelift"
+  type = "PLAN"
+}
