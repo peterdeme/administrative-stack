1cd28736e2e82b588bcbe35c7b424dbb8b53fceb
Peter Deme <snypox@gmail.com>
2025-10-20 12:38:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,10 @@ provider "spacelift" {
   api_key_endpoint = "https://peterdeme.app.spacelift.io"
 }
 
+resource "random_pet" "pet1" {
+}
+
 resource "spacelift_context" "ctx1" {
-  name     = "Context in childspace2"
+  name     = "Context ${random_pet.pet1.id}"
   space_id = "sibling2-01K80HYFVQE1KX57Z628KDE9XJ"
 }
