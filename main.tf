4211c83a04431a59025b65252b252d2b134bdff0
Peter Deme <snypox@gmail.com>
2025-10-20 16:08:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,21 +10,8 @@ provider "spacelift" {
   api_key_endpoint = "https://peterdeme.app.spacelift.tf"
 }
 
-# Parent stack
-resource "spacelift_stack" "infra" {
-  name       = "Base infrastructure from TF Provider"
-  repository = "terraform-testack2"
-  branch     = "main"
-  autodeploy = true
-}
-
-resource "spacelift_space" "development" {
-  name = "Space from TF module"
-  parent_space_id = "root"
-  description = "This a child of the root space - made in a TF module"
-}
-
 resource "spacelift_context" "ctx" {
   description = "A context made in the tf provider"
   name        = "Context from TF provider"
+  space_id    = "childspace1-01K71Y0MKV30KCCMPJQ32783RW"
 }
