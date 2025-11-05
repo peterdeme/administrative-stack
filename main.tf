a6a5d04dfb3a421443ec7a0f2f521e02bd5e1958
Peter Deme <snypox@gmail.com>
2025-11-05 19:11:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,29 +16,5 @@ resource "random_pet" "pet1" {
 
 resource "spacelift_context" "ctx1" {
   name     = "Context - ${random_pet.pet1.id}"
-  space_id = "sibling1-01K80HXRZHWHQYFREDD20HN2Y0"
+  space_id = "sibling2-01K80HYFVQE1KX57Z628KDE9XJ"
 }
-
-#resource "spacelift_policy" "no-weekend-deploys" {
-#  name = "Policy ${random_pet.pet1.id}"
-#  body = "package spacelift"
-#  type = "PLAN"
-#  space_id = "dev-01JZT84P5N16MKWSAVYWMFAAB1"
-#}
-
-#data "terraform_remote_state" "deepthought" {
-#  backend = "remote"
-
-#  config = {
-#    hostname     = "spacelift.io"
-#    organization = "peterdeme"
-
-#    workspaces = {
-#      name = "dev"
-#    }
-#  }
-#}
-
-#output "ultimate_answer" {
-#  value = data.terraform_remote_state.deepthought.outputs.name
-#}
